fb1b0567365281843d91f020e90b3d1746284727
dollyzmq <31545032+dollyzmq@users.noreply.github.com>
2017-10-22 23:10:48 -0400

    first

---
+++ application.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+from elasticsearch import Elasticsearch
+import json
+import requests
+
+application = Flask(__name__)
+
+socketio = SocketIO(application)
+
+@application.route('/')
+def hello_world():
+    return render_template('TwitterMap.html')
+
+
+@socketio.on('message')
+def handleConnected(message):
+    # use localhosted elasticsearch
+    es = Elasticsearch()
+    
+    if message == 'Init':
+        results = es.search(index="twitter", size=2000, body={ "query": { "match_all": {} } })
+        print("INIT MAP")
+
+    else:
+        results = es.search(index="twitter", size=2000, body={"query": {"match": {'text':{'query': message}}}})
+        print("SEARCH: " + message)
+    
+
+    # Find locations of each tweet
+    data = {'location': [], 'user': [], 'time': [], 'text': []}
+
+    for result in results['hits']['hits']:
+        for key in data:
+            data[key].append(result['_source'][key])
+    print(data)
+    
+    send(json.dumps(data))
+
+
+if __name__ == '__main__':
+    socketio.run(application, debug=True)
---
+++ twitter_streaming.py
@@ -0,0 +1,60 @@
+# Important the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream 
+from elasticsearch import Elasticsearch
+import requests
+import json
+
+
+# Variables that contains the user credentials to access Twitter API
+access_token = "920108859007684608-UyNMjzKK1WL67bSkbI9nx7xLX4286nV"
+access_token_secret = "nvwDEdY7a6MccvKFuQQvUmwcjadVKB4B7tc6aNwxRG7aw"
+consumer_key = "RKlq1R5ug0JeQooH3VgwYglXa"
+consumer_secret = "xwy29DNnp8SeoUwBdivi5KvW0UWGFctqZwqTZYr9k2ak25giO6"
+
+
+# This is a basic listener that just prints received tweets to stdout
+class StdOutListener(StreamListener):
+
+	def on_error(self, status):
+		#print status
+		pass
+	
+	def on_status(self, status):
+		try:
+			if status.coordinates:
+				#print status
+				tweet = {}
+				tweet['user'] = status.user.screen_name
+				tweet['text'] = status.text
+				tweet['location'] = status.coordinates['coordinates']
+				tweet['time'] = str(status.created_at)
+
+				#Store twitter data into elasticsearch
+				es.index(index = 'twitter', doc_type = 'tweet', body = {
+					'user': tweet['user'],
+					'text': tweet['text'],
+					'location': tweet['location'],
+					'time': tweet['time']
+					})
+
+				#postURL = 'https://search-movies-bqwntmyexehs2j35wwq4tstn4a.us-west-2.es.amazonaws.com' # AWS endpoint
+				#r = requests.post(postURL, json = tweet)
+
+				print(tweet)
+		except Exception as e:
+			print('Error! {0}: {1}'.format(type(e), str(e)))
+
+if __name__ == '__main__':
+	# Create elasticsearch instance
+	es = Elasticsearch()
+
+	# This handles Twitter authetification and the connection to Twitter Streaming API
+	l = StdOutListener()
+	auth = OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	stream = Stream(auth, l)
+
+	stream.filter(track = ['Trump', 'Hillary', 'Sanders', 'Facebook', 'LinkedIn',
+                             'Amazon', 'Google', 'Los Angeles', 'Columbia', 'New York'])
